2c3a66156cc2a74f7ed62a7c4c02f982ac840436
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-10-07 16:29:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // App.js
 import React from "react";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector , useDispatch} from "react-redux";
 import Layoutheader from "./components/layout/Layoutheader";
 import Cart from "./components/cart/Cart";
 import CartItem from "./components/cart/CartItem";
 import Product from "./components/shop/Product";
 //import Ui from "./components/ui/Ui";
+import Notification from "./components/cart/Notification";
+import CartDataHandler from "./components/cart/CartDataHandler";
+import { fetchCartData } from "./components/store/cartAction";
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.cart.isVisible);
-
+  const notification = useSelector((state) => state.ui.notification);
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
   return (
     <div>
       <Layoutheader />
